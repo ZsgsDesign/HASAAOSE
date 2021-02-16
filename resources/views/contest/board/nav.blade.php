dc24ae9c309ac68b59daac66c4bea709fdf27254
ZsgsDesign <zsgsdesign@gmail.com>
2021-02-16 15:00:13 +0800

    remmove clarification and print link, make rank admin only

--- resources/views/contest/board/nav.blade.php
+++ resources/views/contest/board/nav.blade.php
@@ -26,15 +26,15 @@
 <nav-div>
     @if(time() >= strtotime($basic['begin_time']))
         <a href="/contest/{{$cid}}/board/challenge" data-nav="challenge"><nav-item>{{__("contest.inside.topbar.challenge")}}</nav-item></a>
-        <a href="/contest/{{$cid}}/board/rank" data-nav="rank"><nav-item>{{__("contest.inside.topbar.rank")}}</nav-item></a>
         <a href="/contest/{{$cid}}/board/status" data-nav="status"><nav-item>{{__("contest.inside.topbar.status")}}</nav-item></a>
-        <a href="/contest/{{$cid}}/board/clarification" data-nav="clarification"><nav-item>{{__("contest.inside.topbar.clarification")}}</nav-item></a>
-        <a href="/contest/{{$cid}}/board/print" data-nav="print"><nav-item>{{__("contest.inside.topbar.print")}}</nav-item></a>
+        {{-- <a href="/contest/{{$cid}}/board/clarification" data-nav="clarification"><nav-item>{{__("contest.inside.topbar.clarification")}}</nav-item></a> --}}
+        {{-- <a href="/contest/{{$cid}}/board/print" data-nav="print"><nav-item>{{__("contest.inside.topbar.print")}}</nav-item></a> --}}
         @if($basic['practice'])
             <a href="/contest/{{$cid}}/board/analysis" data-nav="analysis"><nav-item>{{__("contest.inside.topbar.analysis")}}</nav-item></a>
         @endif
     @endif
     @if($clearance>2)
+        <a href="/contest/{{$cid}}/board/rank" data-nav="rank"><nav-item>{{__("contest.inside.topbar.rank")}}</nav-item></a>
         <a href="/contest/{{$cid}}/board/admin" data-nav="admin"><nav-item>{{__("contest.inside.topbar.admin")}}</nav-item></a>
     @endif
     <script>
