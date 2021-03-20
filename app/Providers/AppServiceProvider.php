3aeb6181998a49f0c094d1d6fac7fce15bb24d5e
zsgsdesign <zsgsdesign@gmail.com>
2021-03-20 10:59:12 +0800

    fixed config request class not found error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config(['app.url' => request()->root()]);
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -76,7 +76,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', request()->root()),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
